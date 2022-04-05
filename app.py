d09d6804c7402376524449ccaf8e6146e6f2b609
anara01 <anaraerkebaeva2@gmail.com>
2022-04-05 11:36:46 +0300

    adding

--- app.py
+++ app.py
@@ -1,33 +1,26 @@
 from flask import Flask, render_template, request
-import openpyxl
+from database import Good, engine
+from sqlalchemy.orm import sessionmaker
+
 app = Flask(__name__)
-book = openpyxl.open('goods.xlsx', read_only=True)
-sheet = book.active
-goods = []
-i = 1
-while sheet[f'A{i}'].value:
-    goods.append(sheet[f'A{i}'].value)
-    i += 1
-book.close()
+Session = sessionmaker(engine)
 
 @app.route('/')
 def homepage():
-    book = openpyxl.open('goods.xlsx', read_only=True)
-    
+    session = Session()
+    goods = session.query(Good)
+    session.commit()
     return render_template('index.html', goods=goods)
+
+
 @app.route('/add/', methods=["POST"])
 def add():
     good = request.form["good"]
-    goods.append(good)
-    book = openpyxl.open('goods.xlsx')
-    sheet = book.active
-    x = len(goods)
-    sheet[x][0].value = good
-    book.save('goods.xlsx')
-    book.close()
+    session = Session()
+    good_object = Good(name=good)
+    session.add(good_object)
+    session.commit()
     return """
         <h1>Инвентарь пополнен</h1>
         <a href='/'>Домой</a>
     """
-if __name__ == '__main__':
-    app.run(debug=True)
